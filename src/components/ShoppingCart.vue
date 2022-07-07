f70075d6942d07128dc808c4a9307f085fd7e7f6
DanielNegrete <Pablo_29_98@hotmail.com>
2022-07-07 10:01:35 -0500

    Discount feature

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -19,29 +19,42 @@
             <td>{{ item.productCategorie }}</td>
             <td>${{ item.productPrice }}</td>
             <td>{{ item.productQ }}</td>
-            <td><fa icon="xmark" @click="removeCart(item)"></fa></td>
+            <td>
+              <fa icon="xmark" @click="removeCart(item)"></fa>
+            </td>
           </tr>
         </tbody>
       </table><br>
-      <span>Coupon: <input type="text" name="" id=""></span>
-      <button @click="coupon()">Apply</button>
-      <br><br>
-      <span>Total price: ${{ totalPrice }}</span><br><br>
-      <button @click="onPurchase()">Purchase</button>
+      <div v-if="productsInCart == 0">
+        <span>Coupon: <input type="text" name="coupon" id="coupon" v-model="coupon" disabled></span>
+        <button @click="applyCoupon(coupon)" disabled>Apply</button>
+        <br><br>
+        <span>Total price: ${{ totalPrice }}</span><br><br>
+        <button @click="onPurchase()" disabled>Purchase</button>
+      </div>
+      <div v-else>
+        <span>Coupon: <input type="text" name="coupon" id="coupon" v-model="coupon"></span>
+        <button @click="applyCoupon(coupon)">Apply</button>
+        <br><br>
+        <span>Total price: ${{ totalPrice }}</span><br><br>
+        <button @click="onPurchase()">Purchase</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  // eslint-disable-next-line vue/multi-word-component-names
   name: 'ShoppingCart',
   props: {
     msg: String
   },
   data() {
     return {
-      cart: []
+      cart: [],
+      coupons: [{ couponName: 'VUE20', couponDiscount: 0.20 }, { couponName: 'VUE30', couponDiscount: 0.30 }, { couponName: 'VUE50', couponDiscount: 0.50 }],
+      coupon: '',
+      totalPrice: 0,
     }
   },
   created() {
@@ -49,28 +62,46 @@ export default {
     if (products != null) {
       this.cart = JSON.parse(products);
     }
+    let totalPrice = localStorage.getItem("totalPrice");
+    this.totalPrice = JSON.parse(totalPrice);
   },
   computed: {
     productsInCart() {
       return this.cart.reduce((acc, item) => acc + item.productQ, 0);
-    },
-    totalPrice() {
-      return this.cart.reduce((acc, item) => acc + item.productQ * item.productPrice, 0);
     }
   },
   methods: {
     onPurchase() {
-      this.$swal.fire('Thank You!!', 'Your purchase has been completed', 'success');
+      this.$swal.fire('Thank You!!', 'Your purchase has been completed total: ' + this.totalPrice, 'success');
       this.cart = [];
       localStorage.removeItem('cart');
+      localStorage.removeItem('totalPrice');
+      this.totalPrice = 0;
     },
-    removeCart(product){
+    removeCart(product) {
       let iProduct = 0;
       this.cart.forEach((item, i) =>
         item.productId == product.productId ? (iProduct = i) : null
       );
       this.cart.splice(iProduct, 1);
-      this.$toast.error(`Product removed`, { position: 'top-right'});
+      this.$toast.error(`Product removed`, { position: 'top-right' });
+      this.totalPrice = this.cart.reduce((acc, item) => acc + item.productQ * item.productPrice, 0)
+      localStorage.setItem("cart", JSON.stringify(this.cart));
+      localStorage.setItem("cartQ", this.cart.reduce((acc, item) => acc + item.productQ, 0));
+      localStorage.setItem("totalPrice", this.cart.reduce((acc, item) => acc + item.productQ * item.productPrice, 0));
+    },
+    applyCoupon(coupon) {
+      const aCoupon = this.coupons.filter((c) => c.couponName == coupon)[0];
+      if (aCoupon != undefined) {
+        this.coupons.forEach(c => {
+          if (c.couponName == coupon) {
+            this.totalPrice -= this.totalPrice * c.couponDiscount;
+          }
+        });
+        this.$toast.success(`Coupon applied`, { position: 'top-right' });
+      } else {
+        this.$toast.error(`Invalid coupon`, { position: 'top-right' });
+      }
     }
   }
 }
